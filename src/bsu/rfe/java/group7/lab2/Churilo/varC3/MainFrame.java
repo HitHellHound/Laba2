914efb4ba9245e461b7170bd7597822fad8467d1
Ignat Churilo <acb@gmail.com>
2020-10-18 19:51:51 +0300

    Sec

--- src/bsu/rfe/java/group7/lab2/Churilo/varC3/MainFrame.java
+++ src/bsu/rfe/java/group7/lab2/Churilo/varC3/MainFrame.java
@@ -15,6 +15,20 @@ public class MainFrame extends JFrame {
     private ButtonGroup radioButtonsFormulas = new ButtonGroup();
     private ButtonGroup radioButtonsVariable = new ButtonGroup();
 
+    private Box hboxFormulaType = Box.createHorizontalBox();
+    private Box hboxVariableType = Box.createHorizontalBox();
+
+    private int formulaId = 1;
+    private int variableId = 1;
+
+    public Double calculate1 (Double x, Double y, Double z){
+        return (Math.sin(Math.PI * y * y) + Math.log(y * y)) / (Math.sin(Math.PI * z * z) + Math.sin(x) + Math.log(z * z) + x * x + Math.exp(Math.cos(z * x)));
+    }
+
+    public Double calculate2 (Double x, Double y, Double z){
+        return Math.sqrt(Math.sqrt(Math.cos(Math.exp(y)) + Math.exp(y * y) + Math.sqrt(1 / x))) / Math.pow(Math.cos(Math.PI * z * z * z) + Math.log(Math.pow(1 + z, 2)), Math.sin(y));
+    }
+
     public static void main(String[] args){
         MainFrame frame = new MainFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
